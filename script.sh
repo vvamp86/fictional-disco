e508e922c6c08a06ce9db2bee83e4894d1910600
vvamp86 <ryao7970@gmail.com>
2025-03-26 21:10:37 -0400

    new plan for .sh file

---
+++ script.sh
@@ -0,0 +1,18 @@
+# Run initially
+# If works, just commit
+
+
+
+# If leak:
+# "Leak detected on line {{}}. Commit anyways with a report to admin? [y/N]"
+
+# If Y:
+# Push anyways
+# "Report sent to {{}}, commit made successfully"
+
+
+# If N:
+# Ask if saving a backup report:
+# "Do you wish to retain a report of the leak? [Y/n]"
+
+
